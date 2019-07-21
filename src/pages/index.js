e110e0a367005791b3e9238facfe64a11d79d09a
Arti Mathanda <arti.mathanda@simplybusiness.co.uk>
2019-07-21 19:50:18 +0100

    Adds Hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
 
 export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Home!</h1>
-      <p>Hello Frontend Masters!</p>
-      <h2>Read the blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post}></PostPreview>
-      ))}
-    </Layout>
+    <React.Fragment>
+      <Hero />
+      <Layout>
+        <h2>Read the blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post}></PostPreview>
+        ))}
+      </Layout>
+    </React.Fragment>
   );
 };
